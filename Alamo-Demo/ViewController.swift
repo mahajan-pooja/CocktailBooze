b06772ff0b58c422fef1d1c8766b15d01fa55388
Pooja <poojamac@esvs-imac.local>
2018-11-14 16:02:33 -0800

    Initial Commit

---
+++ Alamo-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alamo-Demo
+//
+//  Created by Pooja on 11/14/18.
+//  Copyright © 2018 GenistaBio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
