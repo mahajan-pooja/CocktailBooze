17168fbbaecf152fbc0a4adac41babd325abbc96
Pooja <poojamac@ESVs-iMac.local>
2019-08-15 16:20:47 -0700

    changed theme colors

--- Alamo-Demo/View Controller/Tab 2/VideoCategoryViewController.swift
+++ Alamo-Demo/View Controller/Tab 2/VideoCategoryViewController.swift
@@ -41,7 +41,7 @@ class VideoCategoryViewController: UIViewController, UITableViewDataSource, UITa
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat{
-        return 370
+        return 300
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return arrayAllVideosList.count
@@ -63,7 +63,7 @@ class VideoCategoryViewController: UIViewController, UITableViewDataSource, UITa
         cell.cellView.layer.shadowColor = UIColor.gray.cgColor
         cell.cellView.layer.shadowOpacity = 1
         cell.cellView.layer.shadowOffset = CGSize.zero
-        cell.cellView.layer.shadowRadius = 5
+        cell.cellView.layer.shadowRadius = 2
         
         return cell
     }
--- Alamo-Demo/View Controller/Tab 3/AddRecipeViewController.swift
+++ Alamo-Demo/View Controller/Tab 3/AddRecipeViewController.swift
@@ -11,9 +11,65 @@ import Firebase
 import FirebaseDatabase
 import FirebaseFirestore
 
-class AddRecipeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+class AddRecipeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var mainViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var tblProcedureHeight: NSLayoutConstraint!
     @IBOutlet weak var tblViewAddProcedure: UITableView!
     @IBOutlet weak var tblViewAddIngredient: UITableView!
+    @IBOutlet weak var tblIngredientHeight: NSLayoutConstraint!
+    @IBOutlet weak var btnAddIngredients: UIButton!
+    @IBOutlet weak var addRecipeSection: UIStackView!
+    @IBOutlet weak var addProcedureUIView: UIView!
+    @IBOutlet weak var btnAddProcedure: UIButton!
+    @IBOutlet weak var ingredientsUIView: UIView!
+    @IBOutlet weak var lblProcedure: UILabel!
+    @IBOutlet weak var txtfRecipeName: UITextField!
+    @IBOutlet weak var imgCocktail: UIImageView!
+    @IBOutlet weak var txtfType: UITextField!
+    
+    let imagePicker = UIImagePickerController()
+    var ref: DocumentReference!
+    var imageName: String!
+    var ingredientCount: Int = 1
+    var procedureCount: Int = 1
+    
+    @IBAction func btnAddRecipeAction(_ sender: Any) {
+        addRecipe()
+    }
+    @IBAction func btnAddIngredient(_ sender: Any) {
+        ingredientCount = ingredientCount + 1
+        tblIngredientHeight.constant = tblIngredientHeight.constant+50
+        mainViewHeight.constant = mainViewHeight.constant+50
+        tblViewAddIngredient.reloadData()
+    }
+    
+    @IBAction func btAddProcedureAction(_ sender: Any) {
+        procedureCount = procedureCount + 1
+        tblProcedureHeight.constant = tblProcedureHeight.constant+50
+        mainViewHeight.constant = mainViewHeight.constant+50
+        tblViewAddProcedure.reloadData()
+    }
+    
+    @IBAction func selectImageAction(_ sender: Any) {
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker,animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        btnAddProcedure.layer.cornerRadius = 15
+        btnAddIngredients.layer.cornerRadius = 15
+        
+        ingredientsUIView.layer.cornerRadius = ingredientsUIView.frame.width/15
+        addProcedureUIView.layer.cornerRadius = addProcedureUIView.frame.width/15
+//        tblViewAddProcedure.layer.cornerRadius = tblViewAddProcedure.frame.width/15
+//        tblViewAddIngredient.layer.cornerRadius = tblViewAddIngredient.frame.width/15
+        
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if(tableView == tblViewAddIngredient){
             return ingredientCount
@@ -24,18 +80,15 @@ class AddRecipeViewController: UIViewController, UIImagePickerControllerDelegate
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if(tableView == tblViewAddIngredient){
-        let cell:IngredientsAddTableViewCell = tableView.dequeueReusableCell(withIdentifier: "IngredientsAddTableViewCell", for: indexPath) as! IngredientsAddTableViewCell
-        
-        cell.txtFieldIngredient.tag = indexPath.row+1
-       
-        return cell
-        
+            let cell:IngredientsAddTableViewCell = tableView.dequeueReusableCell(withIdentifier: "IngredientsAddTableViewCell", for: indexPath) as! IngredientsAddTableViewCell
+            //tags starts from 1
+            cell.txtFieldIngredient.tag = indexPath.row+1
+            return cell
         }else{
-    
             let cell:ProcedureTableViewCell = tableView.dequeueReusableCell(withIdentifier: "ProcedureTableViewCell", for: indexPath) as! ProcedureTableViewCell
-    
+            //tags starts from 51
             cell.txtFieldProcedure.tag = indexPath.row+1+50
-    
+            
             return cell
         }
     }
@@ -52,76 +105,21 @@ class AddRecipeViewController: UIViewController, UIImagePickerControllerDelegate
         if (editingStyle == .delete) {
             // handle delete (by removing the data from your array and updating the tableview)
             if(tableView == tblViewAddIngredient){
+                tblIngredientHeight.constant = tblIngredientHeight.constant-50
+                mainViewHeight.constant = mainViewHeight.constant-50
                 ingredientCount = ingredientCount - 1
             }else{
+                tblProcedureHeight.constant = tblProcedureHeight.constant-50
+                mainViewHeight.constant = mainViewHeight.constant-50
                 procedureCount = procedureCount - 1
             }
             tableView.deleteRows(at: [indexPath], with: .fade)
-            
         }
     }
-    let imagePicker = UIImagePickerController()
-    
-    var ref: DocumentReference!
-    var imageName: String!
-    var ingredientCount: Int = 3
-    var procedureCount: Int = 3
-    
-    @IBAction func btnAddRecipeAction(_ sender: Any) {
-        addRecipe()
-        saveToJsonFile()
-    }
-    @IBOutlet weak var btnAddIngredients: UIButton!
-    @IBOutlet weak var addRecipeSection: UIStackView!
-    @IBOutlet weak var addProcedureUIView: UIView!
-    @IBOutlet weak var btnAddProcedure: UIButton!
-    @IBOutlet weak var ingredientsUIView: UIView!
-    @IBOutlet weak var lblProcedure: UILabel!
-    @IBOutlet weak var txtfRecipeName: UITextField!
-    @IBOutlet weak var imgCocktail: UIImageView!
-    @IBOutlet weak var txtfProcedure: UITextField!
-    @IBOutlet weak var txtfType: UITextField!
-    
-    @IBAction func btnAddIngredient(_ sender: Any) {
-        ingredientCount = ingredientCount + 1
-        print("ingredientCount \(ingredientCount)")
-        tblViewAddIngredient.reloadData()
-    }
-    
-    @IBAction func btAddProcedureAction(_ sender: Any) {
-        procedureCount = procedureCount + 1
-        print("procedureCount \(procedureCount)")
-        tblViewAddProcedure.reloadData()
-//        addProcedureUIView.frame.size.height = addProcedureUIView.frame.size.height + 40
-//        let txtField: UITextField = UITextField(frame: CGRect(x: 10, y: addProcedureUIView.frame.size.height-40, width: 300.00, height: 30.00))
-//        txtField.backgroundColor = UIColor.white
-//        txtField.font =  UIFont(name: txtField.font!.fontName, size: 15)
-//        txtField.layer.cornerRadius = 5
-//        txtField.placeholder = " Procedure"
-//        self.addProcedureUIView.addSubview(txtField)
-//        addRecipeSection.frame.origin.y = addRecipeSection.frame.origin.y + 40
-    }
-    
-    @IBAction func selectImageAction(_ sender: Any) {
-        imagePicker.allowsEditing = true
-        imagePicker.sourceType = .photoLibrary
-        
-        present(imagePicker,animated: true, completion: nil)
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //ref = Firestore.firestore().collection("RecipeCollection").document("Recipe")
-        imagePicker.delegate = self
-        btnAddProcedure.layer.cornerRadius = 15
-        btnAddIngredients.layer.cornerRadius = 15
-        //ref = Database.database().reference().child("recipes")
-    }
-    
     func addRecipe(){
         let user = Auth.auth().currentUser
         var email:String!
         if let user = user {
-            //let uid = user.uid
             email = user.email
             print("email - \(email!)")
         }
@@ -142,25 +140,7 @@ class AddRecipeViewController: UIViewController, UIImagePickerControllerDelegate
             }
         }
         
-//                var ingredientsArray: [String] = []
-//                for i in 1...ingredientCount{
-//                    let indexPath = IndexPath.init(row: i-1, section: 0) // Obviously with the correct row/sec
-//                    let itemCell = tblViewAddIngredient.cellForRow(at: indexPath) as! IngredientsAddTableViewCell  // your tableview must be an IBOutlet on your view controller
-//                    ingredientsArray.append(itemCell.txtFieldIngredient.text!)
-//                }
-//
-//                var procedureArray: [String] = []
-//                for i in 1...procedureCount{
-//
-//                    let indexPath = IndexPath.init(row: i-1, section: 0) // Obviously with the correct row/sec
-//                    let itemCell = tblViewAddProcedure.cellForRow(at: indexPath) as! ProcedureTableViewCell  // your tableview must be an IBOutlet on your view controller
-//                    procedureArray.append(itemCell.txtFieldProcedure.text!)
-//                }
-//
-        
-        
-        
-//        print("procedureArray \(procedureArray.count)")
+        //save data on firebase
         let data: [String:Any] = ["recipe-name":txtfRecipeName.text!, "recipe-type":txtfType.text!, "recipe-img":imageName, "ingredients":ingredientsArray, "procedure": procedureArray]
         ref = Firestore.firestore().collection(email).document(txtfRecipeName.text!)
         ref.setData(data){ (error) in
@@ -171,21 +151,7 @@ class AddRecipeViewController: UIViewController, UIImagePickerControllerDelegate
             }
         }
     }
-    func saveToJsonFile() {
-        // Get the url of Persons.json in document directory
-        guard let documentDirectoryUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
-        let fileUrl = documentDirectoryUrl.appendingPathComponent("Recipes.json")
-        let data: [String:Any] = ["recipe-name":txtfRecipeName.text!, "recipe-type":txtfType.text!, "recipe-img":imageName]
-        //let personArray =  [["person": ["name": "Dani", "age": "24"]], ["person": ["name": "ray", "age": "70"]]]
-        
-        // Transform array into data and save it into file
-        do {
-            let jsonData = try JSONSerialization.data(withJSONObject: data, options: [])
-            try jsonData.write(to: fileUrl, options: [])
-        } catch {
-            print(error)
-        }
-    }
+
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let randomInt = Int.random(in: 0..<100000)
         let imgName = "img\(randomInt)"
