f80f9d221afec97eb4b750985f6e0de9f74bd374
Pooja <poojamac@ESVs-iMac.local>
2019-09-09 17:05:33 -0700

    added wine hub

---
+++ Alamo-Demo/View Controller/WineHubCell.swift
@@ -0,0 +1,13 @@
+//
+//  WineHubCell.swift
+//  Alamo-Demo
+//
+//  Created by Pooja on 9/9/19.
+//  Copyright © 2019 GenistaBio. All rights reserved.
+//
+
+import UIKit
+
+class WineHubCell: UICollectionViewCell {
+    
+}
---
+++ Alamo-Demo/View Controller/WineHubVC.swift
@@ -0,0 +1,35 @@
+//
+//  WineHubVC.swift
+//  Alamo-Demo
+//
+//  Created by Pooja on 9/9/19.
+//  Copyright © 2019 GenistaBio. All rights reserved.
+//
+
+import UIKit
+
+class WineHubVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+//            let yourWidth = collectionView.bounds.width/2.0
+//            let yourHeight = yourWidth
+        
+            return CGSize(width: 200, height: 400)
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell:WineHubCell = collectionView.dequeueReusableCell(withReuseIdentifier: "WineHubCell", for: indexPath) as! WineHubCell
+        
+        return cell
+    }
+
+
+}
