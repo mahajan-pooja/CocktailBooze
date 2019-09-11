662d99cb70f9de2b3debc7f11bc48e1a7b3c2243
Pooja <poojamac@ESVs-iMac.local>
2019-09-10 17:02:08 -0700

    added code for wine and beer details screen

---
+++ Alamo-Demo/View Controller/WineBeerDetailVC.swift
@@ -0,0 +1,33 @@
+//
+//  WineBeerDetailVC.swift
+//  Alamo-Demo
+//
+//  Created by Pooja on 9/10/19.
+//  Copyright © 2019 GenistaBio. All rights reserved.
+//
+
+import UIKit
+
+class WineBeerDetailVC: UIViewController {
+
+    @IBOutlet weak var UIViewLblDescExtra: UIView!
+    @IBOutlet weak var UIViewLblDesc: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UIViewLblDesc.layer.cornerRadius = UIViewLblDesc.frame.height/6
+        UIViewLblDesc.layer.shadowColor = UIColor.gray.cgColor
+        UIViewLblDesc.layer.shadowOpacity = 0.8
+        UIViewLblDesc.layer.shadowOffset = CGSize.zero
+        UIViewLblDesc.layer.shadowRadius = 2
+        UIViewLblDesc.layer.masksToBounds = false
+        
+        
+        UIViewLblDescExtra.layer.cornerRadius = UIViewLblDescExtra.frame.height/6
+        UIViewLblDescExtra.layer.shadowColor = UIColor.gray.cgColor
+        UIViewLblDescExtra.layer.shadowOpacity = 0.8
+        UIViewLblDescExtra.layer.shadowOffset = CGSize.zero
+        UIViewLblDescExtra.layer.shadowRadius = 2
+        UIViewLblDescExtra.layer.masksToBounds = false
+    }
+}
